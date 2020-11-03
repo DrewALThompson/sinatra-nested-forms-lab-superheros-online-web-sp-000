098d565fff9ec101019357643f5cf5bb0f086ef0
Drew Thompson <drewallynthompson@gmail.com>
2020-11-03 07:53:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
     
     get '/' do 
-      erb: '../views/super_hero'
+      erb :'../views/super_hero'
+    end
+    
+    get '/team' do 
+      erb :'../views/team'
     end
 
 
